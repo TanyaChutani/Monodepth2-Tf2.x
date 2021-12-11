9fb7f3d83db1d327be82242d5b8497dbfdfb86fb
Tanya <tanyach1997@gmail.com>
2021-12-12 03:50:30 +0530

    Create encoder_utils.py

---
+++ monodepth2/utils/encoder_utils.py
@@ -0,0 +1,46 @@
+import tensorflow as tf
+
+class ResNet_Block(tf.keras.layers.Layer):
+    def __init__(self, 
+                 filters,
+                 downsample):
+        super(ResNet_Block,self).__init__()
+        self.filters = filters
+        self.downsample = downsample
+        self.strides = 2 if self.downsample else 1
+        self.conv_layer = tf.keras.layers.Conv2D(self.filters, 
+                                                 3, 
+                                                 strides=self.strides,
+                                                 padding='same',
+                                                 use_bias=False)
+        self.bn_layer = tf.keras.layers.BatchNormalization(momentum=0.8)
+        self.relu_layer = tf.keras.layers.ReLU()
+        self.conv2_layer = tf.keras.layers.Conv2D(self.filters, 
+                                                 3, 
+                                                 padding='same',
+                                                 use_bias=False)
+        self.bn2_layer = tf.keras.layers.BatchNormalization(momentum=0.8)
+        self.relu2_layer = tf.keras.layers.ReLU()
+        self.conv3_layer = tf.keras.layers.Conv2D(self.filters, 
+                                                 1,
+                                                 strides=self.strides,
+                                                 padding='same',
+                                                 use_bias=False)
+        self.relu3_layer = tf.keras.layers.ReLU()
+
+    def call(self,
+             input_tensor,
+             training=None):
+        x = self.conv_layer(input_tensor)
+        x = self.bn_layer(x, training=training) 
+        x = self.relu_layer(x)
+        x = self.conv2_layer(x)
+        x = self.bn2_layer(x, training=training) 
+        x = self.relu2_layer(x)
+        if self.downsample:
+            y = self.conv3_layer(input_tensor)
+            x = x + y
+        else:
+            x = x + input_tensor
+        x = self.relu3_layer(x)
+        return x
