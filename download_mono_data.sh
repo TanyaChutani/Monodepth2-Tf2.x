92a7da190dc879920ac350adc428f6fdfa5c62d4
Tanya <tanyach1997@gmail.com>
2022-01-02 01:57:50 +0530

    Create download_mono_data.sh

---
+++ download_mono_data.sh
@@ -0,0 +1,8 @@
+# Download dataset link 
+http://www.cvlibs.net/download.php?file=raw_data_downloader.zip
+
+mkdir dataset
+cd dataset
+wget (link received from cvlibs.net)
+unzip raw_data_downloader.zip
+./raw_data_downloader.sh
