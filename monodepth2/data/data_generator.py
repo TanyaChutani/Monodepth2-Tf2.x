df031546544f263ede4d9dfbeefa871795839d0c
Tanya <tanyach1997@gmail.com>
2022-01-08 02:53:01 +0530

    Update data_generator.py

--- monodepth2/data/data_generator.py
+++ monodepth2/data/data_generator.py
@@ -2,15 +2,20 @@ import os
 import numpy as np
 import tensorflow as tf
 
+from monodepth2.utils.data_utils import image_mean
+from monodepth2.utils.kitti_utils import generate_depth_map
+
 class DataGenerator():
     def __init__(self, 
                  data_path, 
                  mode="train", 
-                 batch_size=4, 
+                 batch_size=1, 
+                 dim=(375, 1242), 
                  n_channels=3, 
                  shuffle=True, 
-                 resize_dim=(640, 192)
+                 resize_dim=(192, 640)
         ):
+        self.dim = dim
         self.mode = mode
         self.shuffle = shuffle
         self.n_channels = n_channels
@@ -27,6 +32,29 @@ class DataGenerator():
 
     def __call__(self):
         for i in (self.index):
-            x, y = self.load(os.path.join(self.data_path, 
-                                          self.data[i]))
+            x, y = self.load()
             yield x, y
+
+    def preprocess_image(self, image):
+        image = tf.image.decode_jpeg(image, channels=self.n_channels)
+        image.set_shape([None, None, 3])
+        if self.mode == "train":
+            image = tf.image.resize(image,size=self.resize_dim,
+                                       method=tf.image.ResizeMethod.BILINEAR)
+            image = tf.image.random_crop(image,size=(self.resize_dim[0],self.resize_dim[1],3))
+        image = image/255.0
+        image = tf.cast(image,tf.float32)
+        return image
+    
+    def get_path(files):
+        file_names = dict()
+        side_conversion = 'image_02' if side == 'l' else 'image_03'
+        
+        with open(files, 'r') as f:
+            lines = f.readlines()
+            
+        for line in lines:
+            line_split = line.split(' ')
+            folder = line_split[0]
+            frame_idx = int(line_split[1])
+            side = line_split[2].rstrip() 
